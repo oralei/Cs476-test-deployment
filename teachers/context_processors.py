8ed377055af22eda06a0ec0faf3cd13a6297c582
Saim Munshi <saimmunshi@gmail.com>
2026-03-11 07:31:57 -0600

    context_proccessor added for teacher this files is used to send data such as notification from database and shows it for all view in message panel

--- teachers/context_processors.py
+++ teachers/context_processors.py
@@ -4,7 +4,6 @@ from teachers.models import Notification
 
 def notifications_processor(request):
     if request.user.is_authenticated:
-        # Query notifications for the logged-in user
         user_notifications = Notification.objects.filter(user=request.user).order_by('-created_at')
         return {'notifications': user_notifications}
     return {'notifications': []}
