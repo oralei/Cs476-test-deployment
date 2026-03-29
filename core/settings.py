37489c791cd413e1f6abb6abb3f4be95fbcc895c
oralei <73796709+oralei@users.noreply.github.com>
2026-03-29 17:10:53 -0600

    win516 bug fix - moved logic
    Settings change - Debug mode set to false
    
    Bug fix - removing timezone to avoid server error
    
    Reset back to original to fix deployment
    
    Debug mode back ff
    
    Bug fix - progress routing errors
    
    debug back to true

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,6 @@ load_dotenv()  # 2. Call the tool to "open the vault"
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
